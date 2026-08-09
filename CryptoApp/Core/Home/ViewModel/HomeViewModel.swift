82b9cb481955284173fcceff9cbee888f10a1764
Anurag Kumar Kashyap <anuragsaikashyap@gmail.com>
2026-08-09 16:32:48 +0530

    Added serach functionality to the serachbar

--- CryptoApp/Core/Home/ViewModel/HomeViewModel.swift
+++ CryptoApp/Core/Home/ViewModel/HomeViewModel.swift
@@ -22,15 +22,45 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        dataService.$allCoins.sink(receiveValue: { [weak self] coins in
-            guard let self else { return }
-            DispatchQueue.main.async {
-                self.allCoins = coins
-                    .filter { ($0.marketCapRank ?? 0) > 0 }
-                    .sorted {
-                        ($0.marketCapRank ?? Int.max) < ($1.marketCapRank ?? Int.max)
-                    }
+        //        dataService.$allCoins.sink(receiveValue: { [weak self] coins in
+        //            guard let self else { return }
+        //            DispatchQueue.main.async {
+        //                self.allCoins = coins
+        //                    .filter { ($0.marketCapRank ?? 0) > 0 }
+        //                    .sorted {
+        //                        ($0.marketCapRank ?? Int.max) < ($1.marketCapRank ?? Int.max)
+        //                    }
+        //            }
+        //        }).store(in: &cancellables)
+        
+        ///Here using combineLatest we are subscrinbing to both searchText and allCoins , so anything changes in these the code will run
+        ///Hence the wbove code is no longer needed
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(self.filterCoins)
+        ///This is giving the data after the logic is applied
+            .sink { [weak self] (returnedCoins) in
+                guard let self else {return}
+                DispatchQueue.main.async {
+                    self.allCoins = returnedCoins
+                        .filter { ($0.marketCapRank ?? 0) > 0 }
+                        .sorted {
+                            ($0.marketCapRank ?? Int.max) < ($1.marketCapRank ?? Int.max)
+                        }
+                }
             }
-        }).store(in: &cancellables)
+            .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, startingCoins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else { return startingCoins }
+        
+        let lowercasedText = text.lowercased()
+        return startingCoins.filter { coin in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+        }
     }
 }
